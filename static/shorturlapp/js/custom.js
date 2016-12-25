a11f7013159bbaa6adc02d671f903b23aba4ed06
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2016-12-26 02:26:55 +0530

    ajax request to server

--- static/shorturlapp/js/custom.js
+++ static/shorturlapp/js/custom.js
@@ -4,4 +4,28 @@
 
 $(document).ready(function () {
 
-})
+    $('#reset-btn').click(function () {
+        $('#tobeshort').val("");
+    });
+
+    $('#short-btn').click(function () {
+        var urls = ($('#tobeshort').val());
+        var data = {
+            "longUrls" : urls,
+            "csrfmiddlewaretoken" : $('input[name="csrfmiddlewaretoken"]').val()
+        };
+
+        $.ajax({
+            type : 'POST',
+            url : '',
+            data : data,
+            dataType : 'json',
+            success : function (data) {
+                console.log(data);
+            },
+            error : function () {
+                alert("failed")
+            }
+        });
+    });
+});
