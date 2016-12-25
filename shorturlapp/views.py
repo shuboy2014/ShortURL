c6ada6f9bb9afd19956d94d32dd926067ccd8174
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2016-12-26 00:08:48 +0530

    remove form class

--- shorturlapp/views.py
+++ shorturlapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 import requests
+import json
 
 URL = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAlC-t7RRoIqeVWA0GDoDjRoQ8gFMja-eA'
 
@@ -10,4 +11,12 @@ class Index(View):
         return render(request, "homepage.html", {})
 
     def post(self, request):
-        pass
+        data = {
+            "longUrl": request.POST["longurl"]
+        }
+
+        x = requests.post(url=URL, data=json.dumps(data), headers={
+            'Content-Type': 'application/json'
+        })
+        print x.json()
+        return render(request, "homepage.html", {})
