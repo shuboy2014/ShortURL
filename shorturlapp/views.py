48c7d7854f1066cb7f478eb9fec94b3dd92fe6f7
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2016-12-30 22:43:18 +0530

    hide api key  +  add comment s & testcase of api server error

--- shorturlapp/views.py
+++ shorturlapp/views.py
@@ -3,8 +3,10 @@ from django.http import JsonResponse
 from django.views import View
 import requests
 import json
+import os
 
-URL = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAlC-t7RRoIqeVWA0GDoDjRoQ8gFMja-eA'
+URL = 'https://www.googleapis.com/urlshortener/v1/url?key='
+key = os.environ.get('key', '')
 
 
 class Index(View):
@@ -13,18 +15,30 @@ class Index(View):
 
     def post(self, request):
         urls = request.POST.get("longUrls", "")
+
+        # if key is not in environment variable
+        if key is None:
+            print("Your API is not in Environment Variable!")
+            return JsonResponse({"error": "Api key is not exist!"})
+
         if urls:
             urls = urls.split("\n")
             shorturls = {}
             for index, url in enumerate(urls):
+                # is url is empty or not
                 if url:
                     data = {
                         "longUrl": url
                     }
-                    x = requests.post(url=URL, data=json.dumps(data), headers={
+                    x = requests.post(url=URL + key, data=json.dumps(data), headers={
                         'Content-Type': 'application/json'
                     })
-                    shorturls[str(index)] = x.json()["id"]
+                    response = x.json()
+                    # if url is shortened or not
+                    if response.get("id", ""):
+                        shorturls[str(index)] = response["id"]
+                    else:
+                        shorturls[str(index)] = "Something went wrong with url!"
                 else:
                     shorturls[str(index)] = ""
             return JsonResponse({'status': 'okay', "shorturls": json.dumps(shorturls)})
